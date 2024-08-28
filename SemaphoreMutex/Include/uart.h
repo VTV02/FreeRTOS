405208861cc7d4179fdc0350ed3a8e449526e9e7
VTV02 <93756924+VTV02@users.noreply.github.com>
2024-08-28 08:52:41 +0700

    Add files via upload

---
+++ SemaphoreMutex/Include/uart.h
@@ -0,0 +1,14 @@
+/*
+ * uart.h
+ *
+ *  Created on: Aug 26, 2024
+ *      Author: VTV02
+ */
+
+#ifndef INC_UART_H_
+#define INC_UART_H_
+void USART2_UART_RX_Init(void);
+void USART2_UART_TX_Init(void);
+
+
+#endif /* INC_UART_H_ */
